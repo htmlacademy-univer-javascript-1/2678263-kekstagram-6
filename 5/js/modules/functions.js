d6b1255900230f260026ceeb2cae5a68a0a4d13d
Keks <keks@htmlacademy.ru>
2025-12-01 11:49:09 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/modules/functions.js
@@ -0,0 +1,24 @@
+const isValidStringLength = (string, maxLength) => string.length <= maxLength;
+
+const isPalindrome = (str) => {
+  const strTemp = str.replaceAll(' ', '').toUpperCase();
+  let reversed = '';
+  for(let i = strTemp.length - 1; i >= 0; i--) {
+    reversed += strTemp[i];
+  }
+  return strTemp === reversed;
+};
+
+const getNumberFromString = (str) => {
+  const sourceStr = typeof str === 'number' ? str.toString() : str;
+  let accumulatorNumber = '';
+  for (let i = 0; i < sourceStr.length; i++) {
+    const num = parseInt(sourceStr[i], 10);
+    if (!Number.isNaN(num)) {
+      accumulatorNumber += num;
+    }
+  }
+  return accumulatorNumber === '' ? NaN : Number(accumulatorNumber);
+};
+
+export { isValidStringLength, isPalindrome, getNumberFromString };
